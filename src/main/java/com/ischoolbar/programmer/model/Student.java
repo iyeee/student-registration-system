73000ace5857f2a0a1815f47bb9ec18f3309d827
iyeee <1009699416@qq.com>
2020-06-10 23:31:29 +0800

    update selectCourse,score,a bit student

---
+++ src/main/java/com/ischoolbar/programmer/model/Student.java
@@ -0,0 +1,147 @@
+package com.ischoolbar.programmer.model;
+
+import java.io.InputStream;
+import java.sql.Date;
+
+/**
+ * 
+ * @author llq
+ *学生实体类
+ */
+public class Student {
+	private int id;
+	private String sn;
+	private int num;
+	private String name;
+	private String password;
+	private int grade;
+	private int clazzId;
+	private String status;
+	private String sex;
+	private String identityId;
+	private Date graduateDate;
+	private Date birthday;
+	private String mobile;
+	private String qq;
+	private InputStream photo;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSn() {
+		return sn;
+	}
+
+	public void setSn(String sn) {
+		this.sn = sn;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getGrade() {
+		return grade;
+	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
+	public int getClazzId() {
+		return clazzId;
+	}
+
+	public void setClazzId(int clazzId) {
+		this.clazzId = clazzId;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getIdentityId() {
+		return identityId;
+	}
+
+	public void setIdentityId(String identityId) {
+		this.identityId = identityId;
+	}
+
+	public Date getGraduateDate() {
+		return graduateDate;
+	}
+
+	public void setGraduateDate(Date graduateDate) {
+		this.graduateDate = graduateDate;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getQq() {
+		return qq;
+	}
+
+	public void setQq(String qq) {
+		this.qq = qq;
+	}
+
+	public InputStream getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(InputStream photo) {
+		this.photo = photo;
+	}
+}
